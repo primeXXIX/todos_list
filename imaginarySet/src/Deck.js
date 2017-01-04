b413067d09f247cc968b09a74b72b7101429b444
HJBowers <hjbowers@gmail.com>
2017-01-03 16:23:57 -0800

    moved cards into public folder, created Deck.js

---
+++ imaginarySet/src/Deck.js
@@ -0,0 +1,87 @@
+//array of cards
+module.exports = [
+  {
+    image: process.env.PUBLIC_URL + "/cards/1.gif"
+    number: 1
+    color: "Red"
+    shape: "Squiggle"
+    shading: "Solid"
+  },
+  {
+    image: process.env.PUBLIC_URL + "/cards/2.gif"
+    number: 2
+    color: "Red"
+    shape: "Squiggle"
+    shading: "Solid"
+  },
+  {
+    image: process.env.PUBLIC_URL + "/cards/3.gif"
+    number: 3
+    color: "Red"
+    shape: "Squiggle"
+    shading: "Solid"
+  },
+  {
+    image: process.env.PUBLIC_URL + "/cards/4.gif"
+    number: 1
+    color: "Purple"
+    shape: "Squiggle"
+    shading: "Solid"
+  },
+  {
+    image: process.env.PUBLIC_URL + "/cards/5.gif"
+    number: 2
+    color:  "Purple"
+    shape: "Squiggle"
+    shading: "Solid"
+  },
+  {
+    image: process.env.PUBLIC_URL + "/cards/6.gif"
+    number: 3
+    color:  "Purple"
+    shape: "Squiggle"
+    shading: "Solid"
+  },
+  {
+    image: process.env.PUBLIC_URL + "/cards/7.gif"
+    number: 1
+    color: "Green"
+    shape: "Squiggle"
+    shading: "Solid"
+  },
+  {
+    image: process.env.PUBLIC_URL + "/cards/8.gif"
+    number: 2
+    color: "Green"
+    shape: "Squiggle"
+    shading: "Solid"
+  },
+  {
+    image: process.env.PUBLIC_URL + "/cards/9.gif"
+    number: 3
+    color: "Green"
+    shape: "Squiggle"
+    shading: "Solid"
+  },
+  {
+    image: process.env.PUBLIC_URL + "/cards/10.gif"
+    number: 1
+    color: "Red"
+    shape: "Diamond"
+    shading: "Solid"
+  },
+  {
+    image: process.env.PUBLIC_URL + "/cards/11.gif"
+    number: 2
+    color: "Red"
+    shape: "Diamond"
+    shading: "Solid"
+  },
+  {
+    image: process.env.PUBLIC_URL + "/cards/12.gif"
+    number: 3
+    color: "Red"
+    shape: "Diamond"
+    shading: "Solid"
+  }
+]
